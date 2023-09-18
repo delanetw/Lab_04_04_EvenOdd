eec8d0f143cb3851ab45a089784a6fd27d1d4acc
Tim Delaney <delanetw@mail.uc.edu>
2023-09-17 22:35:30 -0400

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+public class Main
+{
+    public static void main(String[] args)
+    {
+        System.out.println("Enter a number: ");
+        int number = 3;
+        int numToExamine = 2;
+        double m = number % numToExamine;
+        System.out.println("If the output is not zero, the number is odd: " + m);
+
+    }
+}
